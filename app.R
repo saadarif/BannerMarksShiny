e7110ef557ea39faf376056fe8f99ffc1e1c3bd4
Saad Arif <arifsaad@gmail.com>
2023-05-30 16:01:46 +0100

    allow user to select component score column from marksheet

--- app.R
+++ app.R
@@ -10,35 +10,47 @@ library(openxlsx)
 
 ui <- fluidPage(
   titlePanel("Generate Autofilled Banner Import Templates for Mark Upload"),
-  textInput("CW", "1. Enter the course component code as it appears on Banner (and should be the same in your Marksheet)"),
-  strong("2. Upload the template file for the course component as exported from Banner. This file should be in the .xlsx format."),
-  fileInput("bannerTemplate", NULL, buttonLabel = "2. Banner Template Sheet...", accept = c(".xlsx", ".xls")),
-  strong("3. Upload your marksheet. Download this sheet from Moodle."),
+  strong("1. Upload the template file for the course component as exported from Banner. This file should be in the .xlsx format."),
+  fileInput("bannerTemplate", NULL, buttonLabel = "Banner Template Sheet...", accept = c(".xlsx", ".xls")),
+  strong("2. Upload your marksheet. Download this sheet from Moodle."),
   br(),
   em("IMPORTANT: Don't Change anything in the downloaded sheet except the headers for the CW scores, that match their component name on Banner (eg. CWS2WEEk6)."),
   br(),
   em("I also assume the marks are out of 100 (or a percent) for each component.  Save the file as .xlsx"),
-  fileInput("scoreSheet", NULL, buttonLabel = "3. Your Marksheet...", accept = c(".xlsx", ".xls")),
-  strong("4. Download the file using the button below. This file should be able to be imported into Banner as is!!!"),
+  fileInput("scoreSheet", NULL, buttonLabel = "Your Marksheet...", accept = c(".xlsx", ".xls")),
   br(),
+  uiOutput("u_selector"),
+  br(),
+  strong("4. Download the file using the button below. This file should be able to be imported into Banner as is!!!"),
   downloadButton("download1", label = "4. Download your .xlxs file")
 )
 
 
 server <- function(input, output, session) {
   
-  dataset <- reactive({
-    req(input$CW)
-    if(is.null(input$bannerTemplate))
-      return(NULL)
+  marksheet <- reactive({
     if(is.null(input$scoreSheet))
       return(NULL)
+      ss <- read_excel(input$scoreSheet$datapath, 1, na=c("-",""))
+  })
+  
+  #selecting score columns from Marksheet 
+  output$u_selector <- renderUI({
+    df_local <- req(marksheet())
+    selectInput("user_selected","3. Select the column (from your marksheet) containing scores for this component:",
+                choices=names(df_local),
+                selected = names(df_local)[[1]])
+  })
+  
+
+  dataset <- reactive({
+    if(is.null(input$bannerTemplate))
+      return(NULL)
     bt <-read_excel(input$bannerTemplate$datapath, 1 )
-    ss <- read_excel(input$scoreSheet$datapath, 1, na=c("-",""))
-    CWComp = as.name(input$CW)
-    for(id in ss$Username) {
-      if(!(is.na(ss[CWComp][ss["Username"] == id]))) {
-        bt$Score[bt["Student ID"] == id] = round(ss[CWComp][ss["Username"] == id],digits=1)
+    CWComp = req(input$user_selected)
+    for(id in marksheet()$Username) {
+      if(!(is.na(marksheet()[[CWComp]][marksheet()["Username"] == id]))) {
+        bt$Score[bt["Student ID"] == id] = round(marksheet()[[CWComp]][marksheet()["Username"] == id],digits=1)
       }
       else{
       bt$Score[bt["Student ID"] == id] = 0
@@ -49,7 +61,7 @@ server <- function(input, output, session) {
     bt
   })
   
-  
+  #Download file
   output$download1 <- downloadHandler(
     filename = function() {
       paste0(basename(input$bannerTemplate$name))
