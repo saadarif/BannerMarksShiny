5e3ae2bc48942eb6f2179438284d018edf206140
Saad Arif <arifsaad@gmail.com>
2023-12-15 16:09:21 +0000

    Try with shinyapps-deploy-github-action

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,18 @@
-FROM rocker/shiny:4.3.1
-RUN install2.r rsconnect shinyFeedback readxl openxlsx
-WORKDIR /home/BannerMarksShiny
-COPY app.R app.R 
-COPY deploy.R deploy.R
-CMD Rscript deploy.R
+# start from shiny-verse to include both the shiny and tidyverse packages,
+# saving us time assuming that the app to be deployed doesn't have dependencies
+# locked to different versions.
+FROM rocker/shiny-verse:latest
+
+# install rsconnect and renv packages, as well as prerequisite libraries
+RUN apt-get update && apt-get install -y \
+    libssl-dev \
+    libcurl4-openssl-dev
+RUN install2.r rsconnect renv
+
+# copy deploy script to root of the workspace
+COPY deploy.R /deploy.R
+
+# run deploy script, ignoring any .Rprofile files to avoid issues with conflicting
+# library paths.
+# TODO: this may cause issues if the .Rprofile does any setup required for the app to run
+CMD ["Rscript", "--no-init-file", "/deploy.R"]
--- deploy.R
+++ deploy.R
@@ -1,7 +1,68 @@
-# Authenticate
-library(rsconnect)
-setAccountInfo(name = "8ltxdw-saad-arif",
-               token = Sys.getenv("$TOKEN"),
-               secret = Sys.getenv("$SECRET"))
-# Deploy
-deployApp(appFiles = c("app.R"))
+.libPaths("/usr/local/lib/R/site-library")
+cat("loading packages from:", paste("\n - ", .libPaths(), collapse = ""), "\n\n")
+
+# use renv to detect and install required packages.
+if (file.exists("renv.lock")) {
+  renv::restore(prompt = FALSE)
+} else {
+  renv::hydrate()
+}
+
+# set up some helper functions for fetching environment variables
+defined <- function(name) {
+  !is.null(Sys.getenv(name)) && Sys.getenv(name) != ""
+}
+required <- function(name) {
+  if (!defined(name)) {
+    stop("!!! input or environment variable '", name, "' not set")
+  }
+  Sys.getenv(name)
+}
+optional <- function(name) {
+  if (!defined(name)) {
+    return(NULL)
+  }
+  Sys.getenv(name)
+}
+
+# resolve app dir
+# Note that we are likely already executing from the app dir, as
+# github sets the working directory to the workspace path on starting
+# the docker image.
+appDir <- ifelse(
+  defined("INPUT_APPDIR"),
+  required("INPUT_APPDIR"),
+  required("GITHUB_WORKSPACE")
+)
+
+# required inputs
+appName <- required("INPUT_APPNAME")
+accountName <- required("INPUT_ACCOUNTNAME")
+accountToken <- required("INPUT_ACCOUNTTOKEN")
+accountSecret <- required("INPUT_ACCOUNTSECRET")
+
+# optional inputs
+appFiles <- optional("INPUT_APPFILES")
+appFileManifest <- optional("INPUT_APPFILEMANIFEST")
+appTitle <- optional("INPUT_APPTITLE")
+logLevel <- optional("INPUT_LOGLEVEL")
+
+# process appFiles
+if (!is.null(appFiles)) {
+  appFiles <- unlist(strsplit(appFiles, ",", TRUE))
+}
+
+# set up account
+cat("checking account info...")
+rsconnect::setAccountInfo(accountName, accountToken, accountSecret)
+cat(" [OK]\n")
+
+# deploy application
+rsconnect::deployApp(
+  appDir = appDir,
+  appFiles = appFiles,
+  appFileManifest = appFileManifest,
+  appName = appName,
+  appTitle = appTitle,
+  account = accountName
+)
